2cae2567358e2e8f6d4c7a987efb5de4ffc29a3d
dengyouchang <2458429802@qq.com>
2021-04-06 17:33:14 +0800

    spring cloud2021学习

---
+++ cloudalibaba-provider-payment9002/src/main/java/com/atguigu/springcloud/alibaba/NacosProviderDemoApplication.java
@@ -0,0 +1,14 @@
+package com.atguigu.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosProviderDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProviderDemoApplication.class, args);
+    }
+}
